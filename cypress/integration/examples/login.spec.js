26b88fa8d9a6e3a0cccb2b94e8fb0442ed3eef12
Oim <trustoim@gmail.com>
2020-03-05 09:58:00 +0700

    test invalid login

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -1,19 +1,23 @@
 
 describe('Working with inputs', function() {
+    it('Should load login page', () => {
+        cy.visit('http://zero.webappsecurity.com/login.html')
+    });
+
     it('Should fill username', () => {
-        cy.get('#user_login').clear().type('Some Invalid Name')
+        cy.get('#user_login').clear().type('Some Invalid Name', {delay: 50})
     });
 
     it('Should fill password', () => {
-        cy.get('#user_password').clear().type('Some Invalid Password')
+        cy.get('#user_password').clear().type('Some Invalid Password', {delay: 50})
     });
 
     it('Should submit login form', () => {
-        // To Do
+        cy.get('input[name="submit"]').click()
     });
 
     it('Should display error message', () => {
-        // To Do
+        cy.get('.alert-error').should('be.visible')
     });
 });
     
