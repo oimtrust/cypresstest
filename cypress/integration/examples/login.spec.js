a8f4fe620758748fbeaf0c55b6b4b86affcf6495
Oim <trustoim@gmail.com>
2020-03-05 10:58:55 +0700

    override time

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -1,16 +1,25 @@
 
 describe('Working with inputs', function() {
+    it('Should override the current time', () => {
+        const date = new Date(2020, 2, 5).getTime()
+        cy.clock(date)
+        cy.log(date)
+    });
+
     it('Should load login page', () => {
         cy.visit('http://zero.webappsecurity.com/login.html')
-        cy.wait(5000)
+        cy.clearCookies({ log: true })
+        cy.clearLocalStorage('your item', { log: true })
     });
 
     it('Should fill username', () => {
-        cy.get('#user_login').clear().type('Some Invalid Name', {delay: 50})
+        cy.get('#user_login').as('username')
+        cy.get('@username').clear().type('Some Invalid Name', {delay: 50})
     });
 
     it('Should fill password', () => {
-        cy.get('#user_password').clear().type('Some Invalid Password', {delay: 50})
+        cy.get('#user_password').as('password')
+        cy.get('@password').clear().type('Some Invalid Password', {delay: 50})
     });
 
     it('Should mark checkbox', () => {
@@ -23,7 +32,7 @@ describe('Working with inputs', function() {
     });
 
     it('Should display error message', () => {
-        cy.get('.alert-error').should('be.visible')
+        cy.get('.alert-error').should('be.visible').and('contain', 'Login and/or password are wrong.')
     });
 });
     
