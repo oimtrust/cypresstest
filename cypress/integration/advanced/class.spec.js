1fafd3856af11271e4c90fd6a0b5f8365b1eed5e
Fathur Rohim <trustoim@gmail.com>
2020-03-18 20:39:31 +0700

    before and after hooks

--- cypress/integration/advanced/class.spec.js
+++ cypress/integration/advanced/class.spec.js
@@ -19,9 +19,36 @@ class HomePage extends BasePage {
 }
 
 describe('Abstraction with classes', function() {
-    
-    it('Should scroll down and up on the page', function() {
+
+    before(function() {
+        // run before all tests inside describe
+        // setup test or test context
+        // seed or reset the database
         HomePage.loadHomePage()
+    })
+
+    after(function() {
+        // runs after all tests inside describe block are done
+        // test clean up
+        // clean cookies or localStorage
+    })
+
+    beforeEach(function() {
+        // runs before each it block in the describe
+    })
+
+    afterEach(function() {
+        // runs after each it block in the describe
+    })
+    
+    it.skip('1st IT', function() {
+        HomePage.scrollToBottom()
+        HomePage.wait(5000)
+        HomePage.scrollToTop()
+        HomePage.wait(3000)
+    });
+    
+    it('2nd IT', function() {
         HomePage.scrollToBottom()
         HomePage.wait(5000)
         HomePage.scrollToTop()
